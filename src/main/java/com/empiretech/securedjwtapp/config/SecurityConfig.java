c54bde0740fc212d2e2041f981d0bcbd92d01019
Philip Okumu <philipo.okumu@gmail.com>
2022-02-03 16:16:23 +0300

    Added jwt tokens

--- src/main/java/com/empiretech/securedjwtapp/config/SecurityConfig.java
+++ src/main/java/com/empiretech/securedjwtapp/config/SecurityConfig.java
@@ -5,7 +5,10 @@ import com.empiretech.securedjwtapp.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -28,6 +31,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return NoOpPasswordEncoder.getInstance();
         // return new BCryptPasswordEncoder();
 
+    }
+
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+
+    // Disables spring security for particular route provided
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers("/authenticate")
+                .permitAll().anyRequest().authenticated();
+        //         .and().exceptionHandling().and().sessionManagement()
+        //         .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        // http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
     
 }
--- src/main/java/com/empiretech/securedjwtapp/controller/WelcomeController.java
+++ src/main/java/com/empiretech/securedjwtapp/controller/WelcomeController.java
@@ -1,14 +1,40 @@
 package com.empiretech.securedjwtapp.controller;
 
+import com.empiretech.securedjwtapp.Util.JwtUtil;
+import com.empiretech.securedjwtapp.entity.AuthRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    @Autowired
+    private JwtUtil jwtUtil;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @GetMapping("/")
     public String welcome() {
         return "Hello Java Techie Philipo";
+    }
+
+    @PostMapping("/authenticate")
+    public String generateToken(@RequestBody AuthRequest authRequest) throws Exception {
+        try {
+            authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
+        );
+        } catch (Exception e) {
+            throw new Exception("Invalid username or password");
+        }
+
+        return jwtUtil.generateToken(authRequest.getUsername());
     }
     
 }
